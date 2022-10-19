80b99ca89d197b805ad663c719296672a2b4843f
Amarjeet-97 <amarjeetkumardmg8409@gmail.com>
2022-10-19 15:04:38 +0530

    Assignment Completed

--- src/components/App.js
+++ src/components/App.js
@@ -1,2 +1,11 @@
-<p>Now I can render any React component on any DOM node I want using ReactDOM.render</p>
-
+import React from "react";
+import "../styles/App.css";
+const App = () => {
+  return (
+    <p>
+      Now I can render any React component on any DOM node I want using
+      ReactDOM.render
+    </p>
+  );
+};
+export default App;
